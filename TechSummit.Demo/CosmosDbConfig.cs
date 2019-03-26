52640300b2c123b1795c2d966f2c5e5f57c40ec5
filipw <wojcieszyn@gmail.com>
2019-03-26 23:10:08 +0100

    added cosmos db

---
+++ TechSummit.Demo/CosmosDbConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TechSummit.Demo
+{
+    public class CosmosDbConfig
+    {
+        public string Endpoint { get; set; }
+        public string Key { get; set; }
+        public string DbName { get; set; }
+        public string CollectionName { get; set; }
+        public bool IsPartitioningEnabled { get; set; } = true;
+        public HashSet<string> PreferredLocations { get; set; } = new HashSet<string> { "West Europe" };
+    }
+}
--- TechSummit.Demo/Program.cs
+++ TechSummit.Demo/Program.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using WebApiContrib.Core.Results;
 
 namespace TechSummit.Demo
 {
     public class Program
     {
+        private static readonly string[] AllowedLocations = typeof(LocationNames).GetFields(BindingFlags.Static | BindingFlags.Public).Where(x => x.IsLiteral && x.FieldType == typeof(string)).Select(x => (string)x.GetRawConstantValue()).Where(x => x != null).ToArray();
+
+        public static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -25,10 +28,31 @@ namespace TechSummit.Demo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).
+            ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                Configuration = config.Build();
+            }).
             ConfigureServices(s =>
             {
                 s.AddMvcCore().AddJsonFormatters();
                 s.AddRouting();
+
+                var cosmosDbConfig = ConfigurationBinder.Get<CosmosDbConfig>(Configuration.GetSection("CosmosDb"));
+                s.AddSingleton(cosmosDbConfig);
+
+                var client = new DocumentClient(new Uri(cosmosDbConfig.Endpoint), cosmosDbConfig.Key, new ConnectionPolicy());
+                if (cosmosDbConfig.PreferredLocations != null && cosmosDbConfig.PreferredLocations.Any())
+                {
+                    foreach (var location in cosmosDbConfig.PreferredLocations)
+                    {
+                        if (AllowedLocations.Contains(location))
+                        {
+                            client.ConnectionPolicy.PreferredLocations.Add(location);
+                        }
+                    }
+                }
+
+                s.AddSingleton(client);
             }).
             Configure(app =>
             {
@@ -43,11 +67,22 @@ namespace TechSummit.Demo
 
                     r.MapGet("books", async (req, res, routeData) =>
                     {
-                        await res.HttpContext.Ok(new
+                        var client = req.HttpContext.RequestServices.GetRequiredService<DocumentClient>();
+                        var cosmosDbConfig = req.HttpContext.RequestServices.GetRequiredService<CosmosDbConfig>();
+
+                        var collection = UriFactory.CreateDocumentCollectionUri(cosmosDbConfig.DbName, cosmosDbConfig.CollectionName);
+
+                        var query = client.CreateDocumentQuery(collection).AsDocumentQuery();
+                        var entities = new List<Book>();
+                        while (query.HasMoreResults)
                         {
-                            Title = "Kolumbien. Ein Länderporträt",
-                            Author = "Martin Specht"
-                        });
+                            foreach (var entity in await query.ExecuteNextAsync<Book>())
+                            {
+                                entities.Add(entity);
+                            }
+                        }
+
+                        await res.HttpContext.Ok(entities);
                     });
                 });
             });
